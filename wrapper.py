6c3c390f98c3cc669d82fa7b58df01215ee355f1
Seth Akins <sethlinc4@gmail.com>
2024-11-28 15:58:49 -0700

    more things

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,7 @@
 from collections import deque
 from gymnasium import Wrapper
 
+
 class MarkovWrapper(Wrapper):
     def __init__(self, env, delay=0, discretization=0, gamma=1):
         super().__init__(env)
@@ -8,18 +9,26 @@ class MarkovWrapper(Wrapper):
         self.queue = deque(maxlen=delay + 1)
         self.discretization = discretization
         self.gamma = gamma
+        self.done = False
 
     def step(self, action):
-        obs, reward, terminated, truncated, info  = self.env.step(action)
-        for i in range(self.discretization):
-            obs, r, terminated, truncated, info  = self.env.step(action)
-            # reward += (self.gamma ** (i + 1)) * r
-            reward += r
-        self.queue.append((obs, reward, terminated, truncated, info))
+        if not self.done:
+            obs, reward, terminated, truncated, info = self.env.step(action)
+            self.done = terminated or truncated
+            if not self.done:
+                for i in range(self.discretization):
+                    obs, r, terminated, truncated, info = self.env.step(action)
+                    # reward += (self.gamma ** (i + 1)) * r
+                    reward += r
+                    self.done = terminated or truncated
+                    if self.done:
+                        break
+            self.queue.append((obs, reward, terminated, truncated, info))
         return self.queue.popleft()
 
     def reset(self, seed: int = None, **kwargs):
         self.queue.clear()
+        self.done = False
         obs, info = self.env.reset(seed=seed, **kwargs)
         for _ in range(self.delay):
             self.queue.append((obs, 0, False, False, {}))
